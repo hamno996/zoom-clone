ca15b4d85196ecdaea4df3e389117a6c5a434d82
hamno996 <hamnomhsen@gmail.com>
2024-06-27 15:53:58 +0300

    ....

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { tokenProvider } from "@/actions/stream.actions";
+import Loader from "@/components/Loader";
 import { useUser } from "@clerk/nextjs";
 import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-sdk";
 import { ReactNode, useEffect, useState } from "react";
@@ -19,10 +23,14 @@ const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
 				name: user?.username || user?.id,
 				image: user?.imageUrl,
 			},
+			tokenProvider,
 		});
+		setVideoClient(client);
 	}, [user, isLoaded]);
 
-	return <StreamVideo client={videoClient}></StreamVideo>;
+	if (!videoClient) return <Loader />;
+
+	return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 };
 
 export default StreamVideoProvider;
